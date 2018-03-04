013448b14f84c226c0bf819deb54aae4995a1bb1
bhupendra patel <bhupendrapatel1992@gmail.com>
2018-03-04 14:34:18 +0530

    project upload

---
+++ Matromony/src/com/controller/Register.java
@@ -0,0 +1,40 @@
+package com.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Register
+ */
+@WebServlet("/Register")
+public class Register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name=request.getParameter("name");
+		String pass=request.getParameter("pass");
+		String email=request.getParameter("email");
+		String date=request.getParameter("date");
+		String month=request.getParameter("month");
+		String year=request.getParameter("year");
+		String gender=request.getParameter("gender");
+		String subject=request.getParameter("op");
+		System.out.println("hellow   "+date+" "+month+" "+year+" "+gender);
+	}
+
+}
